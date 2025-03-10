6c864b67948013138b3bab8337b616d3dc4e399f
Manima4000 <ferreira.matheusa@ime.eb.br>
2025-03-10 10:46:31 -0300

    Adicionando a tebela users caso não tenha

--- backend/db.js
+++ backend/db.js
@@ -9,7 +9,24 @@ const pool = new Pool({
 });
 
 pool.connect()
-    .then(() => console.log("✅ Conectado ao PostgreSQL!"))
+    .then(() => {
+        console.log("✅ Conectado ao PostgreSQL!");
+
+        const createTableQuery = `
+            CREATE TABLE IF NOT EXISTS users (
+                cpf VARCHAR(11) PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                email VARCHAR(100) NOT NULL UNIQUE,
+                password VARCHAR(100) NOT NULL,
+                birth_date DATE NOT NULL,
+                
+            );
+        `;
+
+        pool.query(createTableQuery)
+            .then(() => console.log("✅ Tabela 'users' criada com sucesso!"))
+            .catch(err => console.error("❌ Erro ao criar tabela:", err));
+    })
     .catch(err => console.error("❌ Erro na conexão:", err));
 
 module.exports = pool;
