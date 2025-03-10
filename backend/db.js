3160f685e403abc3e4b72e8aefd52e1b4a8b304a
MilenaCaruba <milenacaruba@hotmail.com>
2025-03-10 16:25:55 -0300

    Adicionando tabela products

--- backend/db.js
+++ backend/db.js
@@ -12,19 +12,32 @@ pool.connect()
     .then(() => {
         console.log("✅ Conectado ao PostgreSQL!");
 
-        const createTableQuery = `
+        const createUsersTableQuery = `
             CREATE TABLE IF NOT EXISTS users (
                 cpf VARCHAR(11) PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
                 email VARCHAR(100) NOT NULL UNIQUE,
                 password VARCHAR(100) NOT NULL,
-                birth_date DATE NOT NULL,
+                birth_date DATE NOT NULL
                 
             );
         `;
 
-        pool.query(createTableQuery)
+        pool.query(createUsersTableQuery)
             .then(() => console.log("✅ Tabela 'users' criada com sucesso!"))
+            .catch(err => console.error("❌ Erro ao criar tabela:", err));
+
+        const createProductsTableQuery = `
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY, 
+                name VARCHAR(100) NOT NULL, 
+                price DECIMAL(10, 2) NOT NULL,
+                image_url VARCHAR(100) NOT NULL
+            );
+        `;
+
+        pool.query(createProductsTableQuery)
+            .then(() => console.log("✅ Tabela 'products' criada com sucesso!"))
             .catch(err => console.error("❌ Erro ao criar tabela:", err));
     })
     .catch(err => console.error("❌ Erro na conexão:", err));
