05e628df2aedbbaca9ae971c9deaa8ec33c3f158
FioravansoG <fioravanso.giovanna@ime.eb.br>
2025-03-10 16:18:54 -0300

    db orders e orders_item

--- backend/db.js
+++ backend/db.js
@@ -5,27 +5,56 @@ const pool = new Pool({
     host: "localhost",
     database: "labprog3",
     password: "amarelo100",
-    port: 5432, // Porta padrão do PostgreSQL
+    port: 5432, 
 });
 
 pool.connect()
     .then(() => {
         console.log("✅ Conectado ao PostgreSQL!");
 
-        const createTableQuery = `
+        const createUsersTableQuery = `
             CREATE TABLE IF NOT EXISTS users (
                 cpf VARCHAR(11) PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
                 email VARCHAR(100) NOT NULL UNIQUE,
                 password VARCHAR(100) NOT NULL,
-                birth_date DATE NOT NULL,
+                birth_date DATE NOT NULL
                 
             );
         `;
 
-        pool.query(createTableQuery)
+        pool.query(createUsersTableQuery)
             .then(() => console.log("✅ Tabela 'users' criada com sucesso!"))
-            .catch(err => console.error("❌ Erro ao criar tabela:", err));
+            .catch(err => console.error("❌ Erro ao criar tabela 'users':", err));
+
+        const createOrdersTableQuery = `
+            CREATE TABLE IF NOT EXISTS orders (
+                id SERIAL PRIMARY KEY,
+                user_id VARCHAR(11) REFERENCES users(cpf),
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                total NUMERIC(10, 2) NOT NULL
+            );
+        `;
+
+        pool.query(createOrdersTableQuery)
+            .then(() => console.log("✅ Tabela 'orders' criada com sucesso!"))
+            .catch(err => console.error("❌ Erro ao criar tabela 'orders':", err));
+
+        const createOrderItemsTableQuery = `
+            CREATE TABLE IF NOT EXISTS order_items (
+                id SERIAL PRIMARY KEY,
+                order_id INT REFERENCES orders(id),
+                product_id INT REFERENCES products(id),
+                quantity INT NOT NULL,
+                price NUMERIC(10, 2) NOT NULL
+            );
+        `;
+
+        pool.query(createOrderItemsTableQuery)
+            .then(() => console.log("✅ Tabela 'order_items' criada com sucesso!"))
+            .catch(err => console.error("❌ Erro ao criar tabela 'order_items':", err));
+
+        
     })
     .catch(err => console.error("❌ Erro na conexão:", err));
 
