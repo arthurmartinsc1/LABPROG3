a953eefc8d06379614f49da23d67e16e2e5211ce
FioravansoG <fioravanso.giovanna@ime.eb.br>
2025-03-17 19:08:59 -0300

    front sem back tela login

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>McDonald's</h1>
+    </header>
+  );
+}
+
+export default Header;
