13fc6427acebe278402a902e42c58a9c15e9894a
FioravansoG <fioravanso.giovanna@ime.eb.br>
2025-03-18 00:20:18 -0300

    front login sem back, mas com rotas; npm install react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Home from './pages/home';
+import Login from './pages/login';
 import './styles/App.css';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
